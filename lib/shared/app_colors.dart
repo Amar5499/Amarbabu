5ff3027faccb4613118bcf6362465230f5d878e7
Amar5499 <amarbabutharol@gmail.com>
2025-07-21 23:11:21 +0530

    theme update

--- lib/shared/app_colors.dart
+++ lib/shared/app_colors.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const primary = Color(0xFF1A73E8);
-  static const secondary = Color(0xFF34A853);
-  static const background = Color(0xFFF5F5F5);
-  static const textPrimary = Color(0xFF202124);
-  static const textSecondary = Color(0xFF5F6368);
+  static const primary = Color(0xFFFFBE98);
+  static const secondary = Color(0xFFB88AC4);
+  static const background = Color(0xFFFFF8F2);
+  static const textPrimary = Color(0xFF3B3B3B);
+  static const textSecondary = Color(0xFF7A7A7A);
   static const white = Color(0xFFFFFFFF);
 
   static const backgroundGradient = LinearGradient(
     begin: Alignment.topLeft,
     end: Alignment.bottomRight,
     colors: [
-      Color(0xFFE3F2FD),
+      Color(0xFFFFE7D6),
       Color(0xFFFFFFFF),
     ],
   );
